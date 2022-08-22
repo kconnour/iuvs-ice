745eb061e20d79dea6723d3681de20f48e59c73f
Kyle <kyle.d.connour+gitkraken@gmail.com>
2022-08-22 09:25:23 -0600

    Add bug fixes
    The retrieval_longwav_test now seems to work without issue

--- retrieval.py
+++ retrieval.py
@@ -17,13 +17,17 @@ from scipy.io import readsav
 import disort
 import pyrt
 
+#print(disort.disort.__doc__)
+#print(disort.disobrdf.__doc__)
+#raise SystemExit(9)
+
 ##############
 # IUVS data
 ##############
 
 # Load in all the info from the given file
 orbit: int = 3464
-file: int = 7
+file: int = 5
 block = math.floor(orbit / 100) * 100
 
 # Connect to the DB to get the time of year of the orbit
@@ -33,15 +37,17 @@ with psycopg.connect(host='localhost', dbname='iuvs', user='kyle', password='iuv
         sol = cursor.fetchall()[0][0]
 
 # Get the l1b file
-l1b_files = sorted(Path(f'/media/kyle/Samsung_T5/IUVS_data/orbit0{block}').glob(f'*apoapse*{orbit}*muv*.gz'))
+l1b_files = sorted(Path(f'/media/kyle/McDataFace/iuvsdata/production/orbit0{block}').glob(f'*apoapse*{orbit}*muv*.gz'))
 hdul = fits.open(l1b_files[file])
 
 # Load in the reflectance
-reflectance_files = sorted(Path(f'/home/kyle/iuvs/reflectance/orbit0{block}').glob('*'))
-reflectance = np.load(reflectance_files[file])
+ff = np.load('/home/kyle/repos/PyUVS/pyuvs/anc/flatfields/mid-hi-res-flatfield-update.npy')
+reflectance_files = sorted(Path(f'/home/kyle/iuvs/reflectance/orbit0{block}').glob('*nonlinear*'))
+reflectance = np.load(reflectance_files[file]) / ff
 
 # Load in the corrected wavelengths
-wavelengths_files = sorted(Path(f'/home/kyle/iuvs/wavelengths/orbit0{block}/mvn_iuv_wl_apoapse-orbit0{orbit}-muv').glob('*.sav'))
+# TODO: these are the linear ones, not the nonlinear ones
+wavelengths_files = sorted(Path(f'/home/kyle/iuvs/wavelengths/orbit0{block}').glob(f'mvn_iuv_wl_apoapse-orbit0{orbit}-muv*.sav'))
 wavelengths = readsav(wavelengths_files[file])['wavelength_muv'] / 1000  # convert to microns
 
 # Get the data from the l1b file
@@ -77,7 +83,7 @@ gcm_lon = grid.variables['lon'][:]
 gcm_dust_vprof = yearly_gcm.variables['dustref']   # shape: (year, pressure level, lat, lon)
 gcm_ice_vprof = yearly_gcm.variables['cldref']    # shape: (year, pressure level, lat, lon)
 
-gcm_pressure_levels = gcm.variables['pstd']  # shape: (pressure level,)
+gcm_pressure_levels = gcm.variables['pstd']  # shape: (pressure level,). Index 0 is TOA
 gcm_surface_pressure = gcm.variables['ps']   # shape: (season, time of day, lat, lon)
 gcm_surface_temperature = gcm.variables['ts']   # shape: (season, time of day, lat, lon)
 gcm_temperature = gcm.variables['temp']  # shape: (season, time of day, pressure level, lat, lon)
@@ -97,14 +103,13 @@ dust_pmom = np.load('/home/kyle/repos/iuvs-ice/radprop/mars_dust/legendre_coeffi
 dust_wavelengths = np.load('/home/kyle/repos/iuvs-ice/radprop/mars_dust/wavelengths.npy')
 dust_particle_sizes = np.load('/home/kyle/repos/iuvs-ice/radprop/mars_dust/particle_sizes.npy')
 
-# Ice. TODO: use better properties
 ice_cext = np.load('/home/kyle/repos/iuvs-ice/radprop/mars_ice/extinction_cross_section.npy')  # (24, 317)
 ice_csca = np.load('/home/kyle/repos/iuvs-ice/radprop/mars_ice/scattering_cross_section.npy')  # (24, 317)
-#ice_pmom = np.load('/home/kyle/repos/paper3/radprop/mars_dust/legendre_coefficients.npy')  # (129, 24, 317)
+ice_pmom = np.load('/home/kyle/repos/iuvs-ice/radprop/mars_ice/legendre_coefficients.npy')  # (96,)   # For 3 micron particles at 321 nm
 ice_g = np.load('/home/kyle/repos/iuvs-ice/radprop/mars_ice/asymmetry_parameter.npy')
 ice_wavelengths = np.load('/home/kyle/repos/iuvs-ice/radprop/mars_ice/wavelengths.npy')
 ice_particle_sizes = np.load('/home/kyle/repos/iuvs-ice/radprop/mars_ice/particle_sizes.npy')
-ice_pmom = pyrt.decompose_hg(ice_g, 129)
+#ice_pmom = pyrt.decompose_hg(ice_g, 129)
 
 ##############
 # Surface arrays
@@ -117,7 +122,7 @@ bemst = np.zeros(int(0.5*n_streams))
 emust = np.zeros(n_polar)
 rho_accurate = np.zeros((n_polar, n_azimuth))
 rhoq = np.zeros((int(0.5 * n_streams), int(0.5 * n_streams + 1), n_streams))
-rhou = np.zeros((n_streams, int(0.5 * n_streams + 1), n_streams))
+rhou = np.zeros((n_polar, int(0.5 * n_streams + 1), n_streams))
 
 
 def column_density(pressure_profile, temperature_profile, altitude, profargs, tempargs):
@@ -138,7 +143,7 @@ def column_density(pressure_profile, temperature_profile, altitude, profargs, te
     def hydrostatic_profile(alts):
         return pressure_profile(alts, *profargs) / temperature_profile(alts, *tempargs) / Boltzmann
 
-    n = [quad(hydrostatic_profile, altitude[i + 1], altitude[i])[0] for i in range(len(altitude) - 1)]
+    n = [quad(hydrostatic_profile, altitude[i], altitude[i+1])[0] for i in range(len(altitude) - 1)]
     return np.array(n) * 1000
 
 
@@ -169,14 +174,14 @@ def linear_grid_profile(altitude, altitude_grid, profile_grid) -> np.ndarray:
         Raised if any of the inputs do not have the aforementioned desired
         properties.
     """
-    return np.interp(altitude, np.flip(altitude_grid), profile_grid)
+    return np.interp(altitude, altitude_grid, profile_grid)
 
 
 def retrieval(integration: int, spatial_bin: int):
     # Exit if the angles are not retrievable
     if (not solar_zenith_angle_good[integration, spatial_bin]) or solar_zenith_angle[integration, spatial_bin] >= 72 or emission_angle[integration, spatial_bin] >= 72:
         answer = np.zeros((2,)) * np.nan
-        return integration, spatial_bin, answer
+        return integration, spatial_bin, answer, np.nan
 
     pixel_wavs = wavelengths[spatial_bin, :]
 
@@ -194,19 +199,20 @@ def retrieval(integration: int, spatial_bin: int):
     pixel_temperature = gcm_temperature[season_idx, lt_idx, :, lat_idx, lon_idx]
 
     # Get where the "bad" values are
-    bads = np.sum(pixel_temperature.mask)   # The number of missing points
     first_bad = np.argmin(~pixel_temperature.mask)
 
     # Insert the surface values into the temperature and pressure arrays so they're shape (41,) instead of (40,)
     sfc_pressure = gcm_surface_pressure[season_idx, lt_idx, lat_idx, lon_idx]
-    pixel_temperature = np.insert(pixel_temperature, first_bad, gcm_surface_temperature[season_idx, lt_idx, lat_idx, lon_idx])
+    sfc_temperature = gcm_surface_temperature[season_idx, lt_idx, lat_idx, lon_idx]
     pixel_pressure = np.insert(gcm_pressure_levels, first_bad, sfc_pressure)
+    pixel_temperature = np.insert(pixel_temperature, first_bad, sfc_temperature)
 
     # Make the altitudes assuming exponentially decreasing pressure and constant scale height
     z = -np.log(pixel_pressure / sfc_pressure) * 10
 
     # Finally, use these to compute the column density in each "good" layer
-    colden = np.flip(column_density(linear_grid_profile, linear_grid_profile, z, (z, pixel_pressure), (z, pixel_temperature)))[:-bads]  # flip so TOA is first
+    # TODO: this now gives approximately the same value in each grid. Is this right?
+    colden = column_density(linear_grid_profile, linear_grid_profile, z, (z, pixel_pressure), (z, pixel_temperature))[:first_bad]  # flip so TOA is first
 
     ##############
     # Rayleigh scattering
@@ -220,19 +226,54 @@ def retrieval(integration: int, spatial_bin: int):
     ##############
     # Aerosol vertical profiles
     ##############
-    dust_vprof = gcm_dust_vprof[yearly_idx, :-bads, lat_idx, lon_idx]
-    ice_vprof = gcm_ice_vprof[yearly_idx, :-bads, lat_idx, lon_idx]
+    # Get the GCM profiles and normalize them
+    dust_vprof = gcm_dust_vprof[yearly_idx, :first_bad, lat_idx, lon_idx]
+    dust_vprof = dust_vprof / np.sum(dust_vprof)
+    ice_vprof = gcm_ice_vprof[yearly_idx, :first_bad, lat_idx, lon_idx]
+    ice_vprof = ice_vprof / np.sum(ice_vprof)
 
     ##############
     # Surface
     ##############
+    # Make empty arrays. These are fine to be empty with Lambert surfaces and they'll be filled with more complicated surfaces later on
+    bemst_empty = np.zeros(int(0.5 * n_streams))
+    emust_empty = np.zeros(n_polar)
+    rho_accurate_empty = np.zeros((n_polar, n_azimuth))
+    rhoq_empty = np.zeros(
+                 (int(0.5 * n_streams), int(0.5 * n_streams + 1), n_streams))
+    rhou_empty = np.zeros((n_polar, int(0.5 * n_streams + 1), n_streams))
+
+    # For Lambert
     clancy = np.linspace(0.01, 0.015, num=100)
     clancy_wavs = np.linspace(0.2, 0.3, num=100)
     sfc = np.interp(wavelengths, clancy_wavs, clancy)
 
+    # For Hapke HG2. The idea is the make arrays that are (n_wavelengths, array_shape). This way I can compute them once for each wavelength
+    # and each pixel, and then just call them once.
+    wolff_hapke = np.interp(pixel_wavs, np.linspace(0.258, 0.32, num=100), np.linspace(0.07, 0.095, num=100))
+    bemst = np.zeros((19,) + bemst_empty.shape)
+    emust = np.zeros((19,) + emust_empty.shape)
+    rho_accurate = np.zeros((19,) + rho_accurate_empty.shape)
+    rhoq = np.zeros((19,) + rhoq_empty.shape)
+    rhou = np.zeros((19,) + rhou_empty.shape)
+
+    # Make the surface arrays just once for Hapke HG2
+    for counter, foobar in enumerate(wolff_hapke):
+        brdf_arg = np.array([0.8, 0.06, foobar, 0.3, 0.45, 20])
+        _rhoq, _rhou, _emust, _bemst, _rho_accurate = \
+            disort.disobrdf(True, mu[integration, spatial_bin], np.pi, mu0[integration, spatial_bin], False, 0.01, False,
+                            rhoq_empty, rhou_empty, emust_empty, bemst_empty, False,
+                            azimuth[integration, spatial_bin], 0, rho_accurate_empty,
+                            6, brdf_arg, 200, nstr=n_streams, numu=1, nphi=1)
+        bemst[counter] = _bemst
+        emust[counter] = _emust
+        rho_accurate[counter] = _rho_accurate
+        rhoq[counter] = _rhoq
+        rhou[counter] = _rhou
+
     wavelength_indices = [0, 1, 2, -3, -2, -1]
 
-    def simulate_tau(guess: np.ndarray) -> float:
+    def simulate_tau(guess: np.ndarray) -> np.ndarray:
         dust_guess = guess[0]
         ice_guess = guess[1]
 
@@ -242,10 +283,11 @@ def retrieval(integration: int, spatial_bin: int):
             ##############
             # Dust FSP
             ##############
-            dust_extinction = pyrt.extinction_ratio_grid(dust_cext, dust_particle_sizes, dust_wavelengths, pixel_wavs[wav_index])
-            dust_reff_grid = np.linspace(1.4, 1.6, num=100)
+            # Before: pixel_wavs[wav_index]. Now I'm scaling it to 240 nm
+            dust_extinction = pyrt.extinction_ratio_grid(dust_cext, dust_particle_sizes, dust_wavelengths, 0.24)
+            dust_reff_grid = np.linspace(1.5, 1.5, num=100)
             dust_z_reff = np.linspace(100, 0, num=100)
-            dust_reff = np.interp(z[:-bads-1], dust_z_reff, dust_reff_grid)   # The "bad" altitudes don't get assigned a particle size
+            dust_reff = np.interp(z[:first_bad], dust_z_reff, dust_reff_grid)   # The "bad" altitudes don't get assigned a particle size
 
             dust_extinction_grid = pyrt.regrid(dust_extinction, dust_particle_sizes, dust_wavelengths, dust_reff, pixel_wavs)
 
@@ -257,17 +299,21 @@ def retrieval(integration: int, spatial_bin: int):
             ##############
             # Ice FSP
             ##############
-            ice_extinction = pyrt.extinction_ratio_grid(ice_cext, ice_particle_sizes, ice_wavelengths, pixel_wavs[wav_index])
-            ice_reff_grid = np.linspace(1.5, 4, num=100)
+            # Before: pixel_wavs[wav_index]. Now I'm scaling it to 240 nm
+            ice_extinction = pyrt.extinction_ratio_grid(ice_cext, ice_particle_sizes, ice_wavelengths, 0.24)
+            ice_reff_grid = np.linspace(3, 3, num=100)
             ice_z_reff = np.linspace(100, 0, num=100)
-            ice_reff = np.interp(z[:-bads-1], ice_z_reff, ice_reff_grid)   # The "bad" altitudes don't get assigned a particle size
+            ice_reff = np.interp(z[:first_bad], ice_z_reff, ice_reff_grid)   # The "bad" altitudes don't get assigned a particle size
 
             ice_extinction_grid = pyrt.regrid(ice_extinction, ice_particle_sizes, ice_wavelengths, ice_reff, pixel_wavs)
 
             ice_od = pyrt.optical_depth(ice_vprof, colden, ice_extinction_grid, ice_guess)
             ice_ssa = pyrt.regrid(ice_csca / ice_cext, ice_particle_sizes, ice_wavelengths, ice_reff, pixel_wavs)
-            ice_asym = pyrt.regrid(ice_g, ice_particle_sizes, ice_wavelengths, ice_reff, pixel_wavs)
-            ice_legendre = pyrt.decompose_hg(ice_asym, 129)
+            #ice_asym = pyrt.regrid(ice_g, ice_particle_sizes, ice_wavelengths, ice_reff, pixel_wavs)
+            #ice_legendre = pyrt.decompose_hg(ice_asym, 129)
+            # For now I only have a 1D array of phase functions so I need to broadcast
+            ice_legendre = np.broadcast_to(ice_pmom, (ice_ssa.shape + ice_pmom.shape))
+            ice_legendre = np.moveaxis(ice_legendre, -1, 0)
             ice_column = pyrt.Column(ice_od, ice_ssa, ice_legendre)
 
             ##############
@@ -296,28 +342,38 @@ def retrieval(integration: int, spatial_bin: int):
             # Call DISORT
             ##############
 
+            # The 2nd option of the 2nd line is LAMBER
             rfldir, rfldn, flup, dfdt, uavg, uu, albmed, trnmed = \
                 disort.disort(True, False, False, False, [False, False, False, False, False],
-                              False, True, True, False,
+                              False, False, True, False,
                               atm.optical_depth[:, wav_index],
                               atm.single_scattering_albedo[:, wav_index],
                               atm.legendre_coefficients[:, :, wav_index],
                               temper, 1, 1, user_od_output,
                               mu0[integration, spatial_bin], 0,
                               mu[integration, spatial_bin], azimuth[integration, spatial_bin],
-                              np.pi, 0, sfc[wav_index], 0, 0, 1, 3400000, h_lyr,
-                              rhoq, rhou, rho_accurate, bemst, emust,
+                              np.pi, 0, wolff_hapke[wav_index], 0, 0, 1, 3400000, h_lyr,
+                              rhoq[wav_index], rhou[wav_index], rho_accurate[wav_index], bemst[wav_index], emust[wav_index],
                               0, '', direct_beam_flux,
                               diffuse_down_flux, diffuse_up_flux, flux_divergence,
                               mean_intensity, intensity, albedo_medium,
                               transmissivity_medium, maxcmu=n_streams, maxulv=n_user_levels, maxmom=128)
             simulated_toa_reflectance[counter] = uu[0, 0, 0]
+        return simulated_toa_reflectance
 
+    def find_best_fit(guess: np.ndarray):
+        simulated_toa_reflectance = simulate_tau(guess)
+        print(f'{reflectance[integration, spatial_bin, wavelength_indices]} \n'
+              f'{simulated_toa_reflectance} \n'
+              f'{guess}')
         return np.sum((simulated_toa_reflectance - reflectance[integration, spatial_bin, wavelength_indices])**2)
 
-    fitted_optical_depth = minimize(simulate_tau, np.array([0.4, 0.2]), method='Nelder-Mead', bounds=((0, 2), (0, 5))).x
-    print(fitted_optical_depth)
-    return integration, spatial_bin, np.array(fitted_optical_depth)
+    fitted_optical_depth = minimize(find_best_fit, np.array([0.4, 0.3]), method='Nelder-Mead', bounds=((0, 0.8), (0, 1.5))).x
+    solution = np.array(fitted_optical_depth)
+    sim = simulate_tau(solution)
+    chi_squared = np.sum((reflectance[integration, spatial_bin, wavelength_indices] - sim)**2 / sim)
+    print(f'answer={fitted_optical_depth}')
+    return integration, spatial_bin, solution, chi_squared
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -325,9 +381,12 @@ def retrieval(integration: int, spatial_bin: int):
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 memmap_filename_dust = os.path.join(mkdtemp(), 'myNewFileDust.dat')
 memmap_filename_ice = os.path.join(mkdtemp(), 'myNewFileIce.dat')
+memmap_filename_chisq = os.path.join(mkdtemp(), 'myNewFileChiSquared.dat')
 retrieved_dust = np.memmap(memmap_filename_dust, dtype=float,
                            shape=reflectance.shape[:-1], mode='w+')
 retrieved_ice = np.memmap(memmap_filename_ice, dtype=float,
+                           shape=reflectance.shape[:-1], mode='w+')
+retrieved_chi_squared = np.memmap(memmap_filename_chisq, dtype=float,
                            shape=reflectance.shape[:-1], mode='w+')
 
 
@@ -335,8 +394,10 @@ def make_answer(inp):
     integration = inp[0]
     position = inp[1]
     answer = inp[2]
+    chisq = inp[3]
     retrieved_dust[integration, position] = answer[0]
     retrieved_ice[integration, position] = answer[1]
+    retrieved_chi_squared[integration, position] = chisq
 
 
 t0 = time.time()
@@ -345,15 +406,18 @@ pool = mp.Pool(n_cpus - 2)   # save one/two just to be safe. Some say it's faste
 
 # NOTE: if there are any issues in the argument of apply_async (here,
 # retrieve_ssa), it'll break out of that and move on to the next iteration.
-#for integ in range(reflectance.shape[0]):
 # TODO: UPDATE THIS TO RUN OVER THE FULL RANGE OF INTEGRATIONS
 for integ in range(reflectance.shape[0]):
+#for integ in [35, 36, 37, 38]:
     for posit in range(reflectance.shape[1]):
+    #for posit in range(100, 133):
         pool.apply_async(retrieval, args=(integ, posit), callback=make_answer)
+        #retrieval(integ, posit)
 # https://www.machinelearningplus.com/python/parallel-processing-python/
 pool.close()
 pool.join()  # I guess this postpones further code execution until the queue is finished
-np.save(f'/home/kyle/iuvs/retrievals/orbit03400/orbit{orbit}-{file}-dust.npy', retrieved_dust)
-np.save(f'/home/kyle/iuvs/retrievals/orbit03400/orbit{orbit}-{file}-ice.npy', retrieved_ice)
+np.save(f'/home/kyle/iuvs/retrievals/orbit03400/orbit{orbit}-{file}-dust-updateAug1-hg2real-lasp.npy', retrieved_dust)
+np.save(f'/home/kyle/iuvs/retrievals/orbit03400/orbit{orbit}-{file}-ice-updateAug1-hg2real-lasp.npy', retrieved_ice)
+np.save(f'/home/kyle/iuvs/retrievals/orbit03400/orbit{orbit}-{file}-chi_squared-updateAug1-hg2real-lasp.npy', retrieved_chi_squared)
 t1 = time.time()
 print(t1-t0)
